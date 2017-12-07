8272da2d3d789f8ab7605574654094813d73f973
eric920405 <eric21944998@gmail.com>
2017-12-07 22:08:12 +0800

    ->

--- html/js/data.js
+++ html/js/data.js
@@ -1,13 +1,3 @@
 if(data != "1.1.2"){
-	swal({
-		title:"更新?",
-		text:"立即更新\"1.1.2\"版本。",
-		icon:"warning",
-		dangerMode:true,
-		buttons:["取消","確定"]
-	}).then((value) => {
-		if(value){
-			location = "htpps://build.phonegap.com/apps/2911901/install/xb6NoLS3mNwwbMWrMcdT";
-		}
-	});
+	$("head").append("<script>swal({title:\"更新?\",text:\"立即更新\"1.1.2\"版本。\",icon:\"warning\",dangerMode:true,buttons:[\"取消\",\"確定\"]}).then((value)=>{if(value){location = \"htpps://build.phonegap.com/apps/2911901/install/xb6NoLS3mNwwbMWrMcdT\";}});</script>")
 }
