1edad2072a93bf2ba3688af133967fad11875921
eric920405 <eric21944998@gmail.com>
2017-12-07 21:12:57 +0800

    update version 1.1.2 beta

---
+++ html/js/data.js
@@ -0,0 +1,13 @@
+if(data!+"1.1.2 beta"){
+	swal({
+		title:"更新?",
+		text:"立即更新\"1.1.2 beta\"版本。",
+		icon:"warning",
+		dangerMode:true,
+		buttons:["取消","確定"]
+	}).then((value) => {
+		if(value){
+			location = "htpps://build.phonegap.com/apps/2911901/install/xb6NoLS3mNwwbMWrMcdT";
+		}
+	});
+}
